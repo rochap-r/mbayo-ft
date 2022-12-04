a805b812491709d248fb3aab49cf0f1c787f38a6
Rodrigue <rodriguechot@gmail.com>
2022-12-04 11:36:32 +0200

    Ajout de variables about et contact dans AppserviceProvider et MAJ sur show service

--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -17,7 +17,7 @@
 
 <!-- content section -->
 @section('content')
-    <div class="container-fluid bg-primary py-5  bg-header" >
+    <div class="container-fluid bg-primary py-5  bg-header" style="margin-bottom: 60px">
         <div class="row py-5">
             <div class="col-12 pt-lg-5 mt-lg-5 text-center">
                 <h1 class="display-4 text-white animated zoomIn">{{ $service->title }}</h1>
