7a74c411368c1760f6847915d729a0a848dece46
Rodrigue <rodriguechot@gmail.com>
2022-12-04 11:30:54 +0200

    Ajout de variables about et contact dans AppserviceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\AboutConfig;
+use App\Models\ContactConfig;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +28,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        
+        //les données de contact
+        $contact=ContactConfig::find(1);
+        View::share('contact',$contact);
+
+        //les données d'apropos
+        $about=AboutConfig::find(1);
+        View::share('about',$about);
     }
 }
